741e50333b1989691c12450f1ed85c248ebec0f2
Magdalena Rother <magdalena.rother@moccu.com>
2017-02-01 11:21:41 +0100

    Add settings for mockapp

---
+++ legaltext/tests/resources/mockapp/mockapp_settings.py
@@ -0,0 +1,62 @@
+import tempfile
+
+
+DEBUG = True
+
+SECRET_KEY = 'testing'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+
+    'markymark',
+    'legaltext',
+    'legaltext.tests.resources.mockapp',
+)
+
+ROOT_URLCONF = 'legaltext.tests.resources.mockapp.urls'
+
+SITE_ID = 1
+LANGUAGES = (('en-us', 'en-us'),)
+
+MEDIA_ROOT = tempfile.mkdtemp()
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = tempfile.mkdtemp()
+STATIC_URL = '/static/'
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
