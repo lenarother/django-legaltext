601a179633ad10f70b91249840b188b8030d6a85
Magdalena Rother <magdalena.rother@moccu.com>
2017-02-24 15:41:14 +0100

    Raise 404 when requesting non existing legaltext slug

--- legaltext/tests/tests/legaltext/test_views.py
+++ legaltext/tests/tests/legaltext/test_views.py
@@ -3,14 +3,24 @@ from django.core.urlresolvers import reverse
 
 from legaltext.models import LegalTextVersion
 
+from legaltext.tests.factories.legaltext import LegalTextFactory
+
 
 @pytest.mark.django_db
 class TestLegaltextView:
 
     def test_get(self, client):
-        url = reverse('legaltext', kwargs={'slug': 'test-foo-bar'})
+        test_slug = 'test-foo-bar'
+        LegalTextFactory.create(slug=test_slug)
+        url = reverse('legaltext', kwargs={'slug': test_slug})
         response = client.get(url)
 
         assert response.status_code == 200
         assert isinstance(response.context['current_version'], LegalTextVersion)
-        assert response.context['current_version'].legaltext.slug == 'test-foo-bar'
+        assert response.context['current_version'].legaltext.slug == test_slug
+
+    def test_get_non_existing_legaltext(self, client):
+        url = reverse('legaltext', kwargs={'slug': 'no-such-legaltext'})
+        response = client.get(url)
+
+        assert response.status_code == 404
--- legaltext/views.py
+++ legaltext/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.views.generic import TemplateView
 
 from .models import LegalText
@@ -9,12 +10,14 @@ class LegaltextView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(LegaltextView, self).get_context_data(**kwargs)
-        try:
-            slug = self.kwargs['slug']
-        except KeyError:
-            slug = 'default'
+        slug = self.kwargs['slug']
 
-        legaltext_version = LegalText.current_version(slug)
+        try:
+            legaltext = LegalText.objects.get(slug=slug)
+            legaltext_version = legaltext.get_current_version()
+        except LegalText.DoesNotExist:
+            raise Http404
+
         context.update({'current_version': legaltext_version})
         context.update({'current_version_content': legaltext_version.get_content()})
 
