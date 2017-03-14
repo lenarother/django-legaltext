3dc0e73cc32c9d116dd288efad4a14791c73bc5a
Magdalena Rother <magdalena.rother@moccu.com>
2017-03-14 09:38:14 +0100

    #12: Remove unused code

--- legaltext/fields.py
+++ legaltext/fields.py
@@ -1,26 +1,7 @@
-import pickle
-from django import forms
 from django.db import models
 from django.utils.deconstruct import deconstructible
-from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext_lazy as _
 
 from .models import LegalText, LegalTextVersion
-
-
-def add_legaltext_checkboxes(fields, slug):
-    """
-    Fetch current version of legal text with given slug
-    Adds all its checkboxes to formular fields
-
-    Args:
-        formular fields (self.fields in form __init__)
-        legal text slug
-    """
-    version = LegalText.current_version(slug)
-    for counter, checkbox in enumerate(version.checkboxtextversion_set.all()):
-        field_name = 'Checkbox_{}_{}'.format(counter, version.pk)
-        fields[field_name] = LegalTextCheckboxFormField(checkbox)
 
 
 @deconstructible
@@ -31,67 +12,6 @@ class CurrentLegalText(object):
 
     def __call__(self):
         return LegalText.current_version(self.slug)
-
-
-class LegalTextCheckboxFormField(forms.BooleanField):
-
-    def __init__(self, checkbox, *args, **kwargs):
-        self.checkbox = checkbox
-        super().__init__(*args, **kwargs)
-
-    label = property(
-        lambda s: mark_safe(s.checkbox.get_content()),
-        lambda s, v: v
-    )
-
-    def prepare_value(self, value):
-        return bool(value)
-
-    def to_python(self, value):
-        if not value or value in self.empty_values:
-            return None
-        return self.checkbox
-
-
-class MultipleCheckboxWidget(forms.widgets.MultiWidget):
-
-    def __init__(self, checkboxes, attrs=None):
-        self.checkboxes = checkboxes
-        widgets = [forms.CheckboxInput() for checkbox in checkboxes]
-        super(MultipleCheckboxWidget, self).__init__(widgets, attrs)
-
-    def decompress(self, value):
-        if value:
-            return pickle.loads(value)
-        else:
-            return [False for checkbox in self.checkboxes]
-
-    def format_output(self, rendered_widgets):
-        # This will work until django 1.10.
-        # In django 1.11 format_output is removed
-        # https://docs.djangoproject.com/en/dev/releases/1.11/
-        result = ''
-        for x, y in zip(rendered_widgets, self.checkboxes):
-            result += u'<p class="placewidget">%s %s</p>' % (
-                x, y.get_content())
-        return mark_safe(result)
-
-
-class MultiCheckboxFormField(forms.fields.MultiValueField):
-
-    def __init__(self, slug=None, to=LegalTextVersion, **kwargs):
-        error_messages = {'incomplete': _('This field is required')}
-        text = LegalText.current_version(slug)
-        checkboxes = text.checkboxtextversion_set.all()
-        fields_list = [forms.BooleanField() for checkbox in checkboxes]
-        fields = tuple(fields_list)
-        self.widget = MultipleCheckboxWidget(checkboxes)
-        super().__init__(
-            error_messages=error_messages, fields=fields,
-            require_all_fields=True, **kwargs)
-
-    def compress(self, data_list):
-        return all(x is True for x in data_list)
 
 
 class LegalTextField(models.ForeignKey):
@@ -111,40 +31,10 @@ class LegalTextField(models.ForeignKey):
         return name, path, args, kwargs
 
     def to_python(self, value):
-        # import ipdb; ipdb.set_trace()
         if not value or value in self.empty_values:
             return None
-        # return self.checkbox
         try:
             LegalTextVersion.objects.get(pk=value)
         except LegalTextVersion.DoesNotExist:
             return None
         return value
-
-
-class CheckboxWidget(forms.widgets.MultiWidget):
-
-    def __init__(self, slug, attrs=None):
-        self.checkboxes = LegalText.current_version(slug).checkboxtextversion_set.all()
-        self.version = LegalText.current_version(slug)
-        widgets = [forms.CheckboxInput() for checkbox in self.checkboxes]
-        super(CheckboxWidget, self).__init__(widgets, attrs)
-
-    def value_from_datadict(self, data, files, name):
-        return self.version.pk
-
-    def decompress(self, value):
-        if value:
-            return [True for checkbox in self.checkboxes]
-        else:
-            return [None for checkbox in self.checkboxes]
-
-    def format_output(self, rendered_widgets):
-        # This will work until django 1.10.
-        # In django 1.11 format_output is removed
-        # https://docs.djangoproject.com/en/dev/releases/1.11/
-        result = ''
-        for x, y in zip(rendered_widgets, self.checkboxes):
-            result += u'<p class="placewidget">%s %s</p>' % (
-                x, y.get_content())
-        return mark_safe(result)
