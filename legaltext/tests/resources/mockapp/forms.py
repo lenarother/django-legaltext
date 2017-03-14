dc917e6dea3f473f75f9bd1826c00eb3dd46b0aa
Magdalena Rother <magdalena.rother@moccu.com>
2017-03-14 09:32:11 +0100

    #12: Use MultiWidget to represent checkboxes of legaltext version

--- legaltext/tests/resources/mockapp/forms.py
+++ legaltext/tests/resources/mockapp/forms.py
@@ -1,8 +1,6 @@
 from django.forms import ModelForm
 
-from legaltext.fields import (CheckboxWidget, LegalTextCheckboxFormField,
-                              MultiCheckboxFormField, add_legaltext_checkboxes)
-from legaltext.models import LegalText
+from legaltext.widgets import CheckboxWidget
 
 from .constants import MOCKAPP_PRIVACY_SLUG, MOCKAPP_TERMS_SLUG
 from .models import MockSurveyParticipant
@@ -12,22 +10,11 @@ class MockappParticipationForm(ModelForm):
 
     class Meta:
         model = MockSurveyParticipant
-        exclude = ()
-        fields = (
-            'name',
-            'accepted_privacy',
-            'accepted_terms',
-        )
+        fields = ('name', 'accepted_privacy', 'accepted_terms')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # # add single checkbox with known field name
-        # legaltext_version = LegalText.current_version(MOCKAPP_TERMS_SLUG)
-        # terms_checkbox = legaltext_version.checkboxtextversion_set.first()
-        # self.fields['accepted_terms'] = LegalTextCheckboxFormField(terms_checkbox)
-
-        # # add all fields automatically
-        # add_legaltext_checkboxes(self.fields, MOCKAPP_PRIVACY_SLUG)
-        # self.fields['privacy'] = MultiCheckboxFormField(MOCKAPP_PRIVACY_SLUG)
         self.fields['accepted_privacy'].widget = CheckboxWidget(MOCKAPP_PRIVACY_SLUG)
+        self.fields['accepted_privacy'].required = True
         self.fields['accepted_terms'].widget = CheckboxWidget(MOCKAPP_TERMS_SLUG)
+        self.fields['accepted_terms'].required = True
---
+++ legaltext/widgets.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.utils.safestring import mark_safe
+
+from .models import LegalText
+
+
+class CheckboxWidget(forms.widgets.MultiWidget):
+
+    def __init__(self, slug, attrs=None):
+        self.checkboxes = LegalText.current_version(slug).checkboxtextversion_set.all()
+        self.version = LegalText.current_version(slug)
+        widgets = [forms.CheckboxInput() for checkbox in self.checkboxes]
+        super(CheckboxWidget, self).__init__(widgets, attrs)
+
+    def value_from_datadict(self, data, files, name):
+        for checkbox_counter in range(len(self.checkboxes)):
+            chackbox_name = '{}_{}'.format(name, checkbox_counter)
+            if data.get(chackbox_name, None) is None:
+                return None
+        return self.version.pk
+
+    def decompress(self, value):
+        if value:
+            return [True for checkbox in self.checkboxes]
+        else:
+            return [None for checkbox in self.checkboxes]
+
+    def format_output(self, rendered_widgets):
+        # This will work until django 1.10.
+        # In django 1.11 format_output is removed
+        # https://docs.djangoproject.com/en/dev/releases/1.11/
+        result = ''
+        for x, y in zip(rendered_widgets, self.checkboxes):
+            result += u'<p class="placewidget">{} {}</p>'.format(x, y.get_content())
+        return mark_safe(result)
