1cd7e10909d2a12e5aec0817f50bac4e7beef2c9
Magdalena Rother <magdalena.rother@moccu.com>
2017-03-16 09:32:43 +0100

    #13: Refactor logic in LegalTextVersionAdminForm

--- legaltext/admin.py
+++ legaltext/admin.py
@@ -79,8 +79,8 @@ class LegalTextVersionAdminForm(forms.ModelForm):
         exclude = []
 
     def __init__(self, *args, **kwargs):
-        if kwargs.get('initial', None) and kwargs.get('initial', None).get('legaltext', None):
-            legaltext_pk = kwargs.get('initial').get('legaltext')
+        legaltext_pk = kwargs.get('initial', {}).get('legaltext')
+        if legaltext_pk:
             previous_text = LegalTextVersion.objects.filter(
                 legaltext=legaltext_pk).first().content
             kwargs['initial'].update({'content': previous_text})
