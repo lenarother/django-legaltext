5aee739e3b27c9e58a077c43e849c74c598a82bd
Stephan Jaekel <steph@rdev.info>
2017-08-04 08:13:59 +0200

    Add support to override view templates.

--- legaltext/views.py
+++ legaltext/views.py
@@ -1,11 +1,17 @@
+from django.conf import settings
 from django.views.generic import DetailView
 
 from .models import LegalText
 
 
 class LegaltextView(DetailView):
-    template_name = 'legaltext/content.html'
     model = LegalText
+
+    def get_template_names(self):
+        template_name = getattr(
+            settings, 'LEGALTEXT_VIEW_TEMPLATE', 'legaltext/content.html')
+        overrides = getattr(settings, 'LEGALTEXT_VIEW_TEMPLATE_OVERRIDES', {})
+        return overrides.get(self.object.slug, template_name)
 
     def get_context_data(self, **kwargs):
         context = super(LegaltextView, self).get_context_data(**kwargs)
--- testing/tests/test_views.py
+++ testing/tests/test_views.py
@@ -2,11 +2,32 @@ import pytest
 from django.core.urlresolvers import reverse
 
 from legaltext.models import LegalTextVersion
+from legaltext.views import LegaltextView
 from testing.factories import LegalTextFactory
 
 
 @pytest.mark.django_db
 class TestLegaltextView:
+
+    def test_get_template_names_default(self):
+        view = LegaltextView()
+        view.object = LegalTextFactory.create(slug='foo')
+        assert view.get_template_names() == 'legaltext/content.html'
+
+    def test_get_template_names_custom(self, settings):
+        settings.LEGALTEXT_VIEW_TEMPLATE = 'bar.html'
+        view = LegaltextView()
+        view.object = LegalTextFactory.create(slug='foo')
+        assert view.get_template_names() == 'bar.html'
+
+    def test_get_template_names_override(self, settings):
+        settings.LEGALTEXT_VIEW_TEMPLATE_OVERRIDES = {'bar': 'baz.html'}
+        view = LegaltextView()
+        view.object = LegalTextFactory.create(slug='foo')
+        assert view.get_template_names() == 'legaltext/content.html'
+        view = LegaltextView()
+        view.object = LegalTextFactory.create(slug='bar')
+        assert view.get_template_names() == 'baz.html'
 
     def test_get(self, client):
         test_slug = 'test-foo-bar'
